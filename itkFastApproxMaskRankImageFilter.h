23fcedc97625a7cd2b5752f4de6175ccb384b282
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-10-17 16:43:50 +0200

    add missing include
    darcs-hash:20061017144350-2fc9d-fab05b4f921819aabb680b6ffa660b58e3b9a84e.gz

--- itkFastApproxMaskRankImageFilter.h
+++ itkFastApproxMaskRankImageFilter.h
@@ -6,6 +6,7 @@
 #include "itkMaskedExternMovingHistogramRankImageFilter.h"
 #include "itkNotImageFilter.h"
 #include "itkMaskNegatedImageFilter.h"
+#include "itkMaskImageFilter.h"
 #include "itkImageFileWriter.h"
 
 namespace itk {
